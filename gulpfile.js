c866120ff4aa03c8193dec37e28c64ae21d22501
liushuai <liushuai@liushuaideMacBook-Pro.local>
2018-07-31 18:10:58 +0800

    add postcss-alias

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ const gulp         = require('gulp');
 const postcss      = require('gulp-postcss');
 const pump         = require('pump');
 
+const alias        = require('postcss-alias');
+
 gulp.task('css', function(cb) {
     const plugins = [
+        alias
     ];
     pump([
         gulp.src('src/*.css'),
