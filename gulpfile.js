66898c4be90661e6b94465bf826fa8dc5c66dea7
liushuai <liushuai@liushuaideMacBook-Pro.local>
2018-07-31 19:34:02 +0800

    precss_test

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,12 @@ const alias        = require('postcss-alias');
 const magician     = require('postcss-font-magician');
 const circle       = require('postcss-circle');
 const triangle     = require('postcss-triangle');
+const precss       = require('precss');
 
 gulp.task('css', function(cb) {
     const plugins = [
         alias,
+        precss(),
         magician,
         circle,
         triangle
